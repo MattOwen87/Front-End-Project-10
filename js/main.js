d79707bdf821a0775151f6d036b0280619c8d0c5
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2019-01-05 17:15:27 +0000

    added api funtionality

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
 
 
 $.ajax({
-  url: 'https://randomuser.me/api/',
+  url: 'https://randomuser.me/api/?results=12&inc=picture,name,email,location',
   dataType: 'json',
   success: function(data) {
+    let employees = '<ul>';
 
+    $.each(data.results, function(i, employee) {
+      employees += '<div class="employeeData"><li class="content"><img src="' + employee.picture.large + '" class="employeeImage"><div class="employeeInfo"><h1>' + employee.name.first + ' ' + employee.name.last + '</h1><a>' + employee.email + '</a><p>' + employee.location.city + '</p></div></div></li>'
+    });
+    employees += '</ul>';
+    $('.content').html(employees);
     console.log(data);
   }
-
 });
