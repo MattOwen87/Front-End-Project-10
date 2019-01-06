5c4315512a659c3a0c6ea28081f0011beafd6caf
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2019-01-06 16:37:46 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-
+const employeeContent = document.getElementById('content');
+const closeModal = document.getElementById('modalClose');
+const modal = document.getElementById('employeeModal');
 
 $.ajax({
-  url: 'https://randomuser.me/api/?results=12&inc=picture,name,email,location',
+  url: 'https://randomuser.me/api/?results=12&nat=gb',
   dataType: 'json',
   success: function(data) {
     let employees = '<ul>';
@@ -10,7 +12,16 @@ $.ajax({
       employees += '<div class="employeeData"><li class="content"><img src="' + employee.picture.large + '" class="employeeImage"><div class="employeeInfo"><h1>' + employee.name.first + ' ' + employee.name.last + '</h1><a>' + employee.email + '</a><p>' + employee.location.city + '</p></div></div></li>'
     });
     employees += '</ul>';
-    $('.content').html(employees);
+    $('#content').html(employees);
     console.log(data);
   }
+  
+});
+
+employeeContent.addEventListener('click', () => {
+  modal.style.display = 'block';
+});
+
+closeModal.addEventListener('click', (event) => {
+  modal.style.display = 'none';
 });
