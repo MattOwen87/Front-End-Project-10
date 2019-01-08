4ae86cf3948a8f101d6c8aa345c641620a7274f8
Matthew Owen Haworth <matthew_haworth@hotmail.co.uk>
2019-01-08 18:51:53 +0000

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const employeeContent = document.getElementById('content');
 const closeModal = document.getElementById('modalClose');
 const modal = document.getElementById('employeeModal');
+let modalEmployee = [' + employee.picture.large + ', ' + employee.name.first + ', ' + employee.email + ', ' + employee.phone + ', ' + employee.location.street + ', ' + employee.location.city + ', ' + employee.location.state + ', ' + employee.location.postcode + ', ' + employee.dob + '  ];
 
 $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=gb',
@@ -9,16 +10,17 @@ $.ajax({
     let employees = '<ul>';
 
     $.each(data.results, function(i, employee) {
-      employees += '<div class="employeeData"><li class="content"><img src="' + employee.picture.large + '" class="employeeImage"><div class="employeeInfo"><h1>' + employee.name.first + ' ' + employee.name.last + '</h1><a>' + employee.email + '</a><p>' + employee.location.city + '</p></div></div></li>'
+      employees += '<li class="employeeData"><div class="content"><img src="' + employee.picture.large + '" class="employeeImage"><div class="employeeInfo"><h1>' + employee.name.first + ' ' + employee.name.last + '</h1><a>' + employee.email + '</a><p>' + employee.location.city + '</p></div></div></li>'
     });
     employees += '</ul>';
     $('#content').html(employees);
     console.log(data);
+
   }
 
 });
 
-employeeContent.addEventListener('click', () => {
+employeeContent.addEventListener('click', (event) => {
   modal.style.display = 'block';
 });
 
